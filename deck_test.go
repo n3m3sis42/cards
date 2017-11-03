7189fa4ecae359ce9e350c6da560e39c61c227ef
n3m3sis42 <n3m3sis42@gmail.com>
2017-11-03 13:40:35 -0400

    added more tests for deck custom type - project complete (for now)

--- deck_test.go
+++ deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
@@ -8,4 +11,29 @@ func TestNewDeck(t *testing.T) {
 	if len(d) != 52 {
 		t.Errorf("Expected deck length of 52, but got %v", len(d))
 	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card to be Ace of Spades, but got %v", d[0])
+	}
+
+	if d[len(d)-1] != "King of Clubs" {
+		t.Errorf("Expected last card to be King of Clubs, but got %v", d[len(d)-1])
+	}
+}
+
+func TestSaveToDeckAndTestNewDeckFromFile(t *testing.T) {
+	// delete any deck files created by previous tests - this is in case a test crashes and leaves one behind
+	os.Remove("_decktesting")
+
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 52 {
+		t.Errorf("Expected 52 cards in deck, got %v", len(loadedDeck))
+	}
+
+	// clean up test deck
+	os.Remove("_decktesting")
 }
